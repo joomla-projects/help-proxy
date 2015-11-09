66ced59a2fa2f7ab6119475aa4c182a4cfad3678
Michael Babker <mbabker@flbab.com>
2015-11-09 09:38:52 -0500

    Add Google Tag Manager code

--- tpl_help/index.php
+++ tpl_help/index.php
@@ -18,7 +18,17 @@ $this->addStylesheet('templates/' . $this->template . '/css/help.css');
 <head>
 	<jdoc:include type="head"/>
 </head>
-<body><a name="Top" id="Top"></a>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T742X9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T742X9');</script>
+<!-- End Google Tag Manager -->
+<a name="Top" id="Top"></a>
 <jdoc:include type="component"/>
 <hr/>
 <a href="<?php echo JUri::getInstance(); ?>#Top">Top</a>
