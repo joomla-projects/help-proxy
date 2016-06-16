f5b643bbbe1ecf9f4647a64cb234c5fbfbd5447b
Michael Babker <mbabker@flbab.com>
2016-06-16 10:05:12 -0500

    Updated GTM code

--- tpl_help/index.php
+++ tpl_help/index.php
@@ -23,12 +23,12 @@ JHtml::_('stylesheet', 'help.css', [], true, false, false);
 	</head>
 	<body>
 		<!-- Google Tag Manager -->
-		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T742X9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NVGP9X" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-T742X9');</script>
+		})(window,document,'script','dataLayer','GTM-NVGP9X');</script>
 		<!-- End Google Tag Manager -->
 		<a name="Top" id="Top"></a>
 		<jdoc:include type="component"/>
